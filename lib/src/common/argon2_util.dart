ee118f7a36abf1e3d797e7ea48d78cc3cc9c206d
Riguz Lee <dr@riguz.com>
2025-03-14 08:22:29 +0800

    move argon compution to vm thread

--- lib/src/common/argon2_util.dart
+++ lib/src/common/argon2_util.dart
@@ -1,10 +1,9 @@
-import 'dart:typed_data';
-
+import 'package:flutter/foundation.dart';
 import 'package:kdbx/kdbx.dart';
 
 class Argon2Util {
   static const argon2 = PointyCastleArgon2();
   static Future<Uint8List> deriveKey(Argon2Arguments args) async {
-    return argon2.argon2Async(args);
+    return compute(argon2.argon2Async, args);
   }
 }
